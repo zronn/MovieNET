a6ce4ec84b5dc275fd9946b92435e1f7c608fc09
Ronen-dev <ronenn.ma@gmail.com>
2017-03-01 16:13:39 +0100

    Tests de création user

--- server_movie/server_movie/Program.cs
+++ server_movie/server_movie/Program.cs
@@ -10,6 +10,27 @@ namespace server_movie
     {
         static void Main(string[] args)
         {
+            // Instanciation du context
+            DataModelContainer context = new DataModelContainer();
+
+            // Instanciation de la classe User et creation d'un user factice
+            User user = new User();
+
+            // var query = context.UserSet.Where(u => u.Login.Contains("r")).ToList();
+
+            // Console.WriteLine(query);
+
+            user.Login = "Badr";
+            user.Password = "azerty123";
+            user.Name = "Taner";
+            user.Surname = "Tava";
+
+            // Add from context user in UserSet collection
+            context.UserSet.Add(user);
+
+            // Add user to database
+            context.SaveChanges();
+
         }
     }
 }
