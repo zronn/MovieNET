a8f3fb2440f157f596d07339aaa83a23ddb4a3af
Ronen-dev <ronenn.ma@gmail.com>
2017-03-05 00:59:54 +0100

    Ajout DAO / BDD

---
+++ MovieNet/MovieNet/MovieDataModel.edmx.sql
@@ -0,0 +1,202 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 03/05/2017 00:55:05
+-- Generated from EDMX file: C:\Users\Zouzma\Source\Repos\MovieNET\MovieNet\MovieNet\MovieDataModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [MovieDatabase];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'UserSet'
+CREATE TABLE [dbo].[UserSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Login] nvarchar(max)  NOT NULL,
+    [Password] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'MovieSet'
+CREATE TABLE [dbo].[MovieSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Title] nvarchar(max)  NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [User_Id] int  NOT NULL,
+    [Type_Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'NoteSet'
+CREATE TABLE [dbo].[NoteSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Value] nvarchar(max)  NOT NULL,
+    [Movie_Id] int  NOT NULL,
+    [User_Id] int  NOT NULL
+);
+GO
+
+-- Creating table 'TypeSet'
+CREATE TABLE [dbo].[TypeSet] (
+    [Id] int IDENTITY(1,1) NOT NULL
+);
+GO
+
+-- Creating table 'CommentSet'
+CREATE TABLE [dbo].[CommentSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [Movie_Id] int  NOT NULL,
+    [User_Id] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'UserSet'
+ALTER TABLE [dbo].[UserSet]
+ADD CONSTRAINT [PK_UserSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'MovieSet'
+ALTER TABLE [dbo].[MovieSet]
+ADD CONSTRAINT [PK_MovieSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'NoteSet'
+ALTER TABLE [dbo].[NoteSet]
+ADD CONSTRAINT [PK_NoteSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'TypeSet'
+ALTER TABLE [dbo].[TypeSet]
+ADD CONSTRAINT [PK_TypeSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'CommentSet'
+ALTER TABLE [dbo].[CommentSet]
+ADD CONSTRAINT [PK_CommentSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [User_Id] in table 'MovieSet'
+ALTER TABLE [dbo].[MovieSet]
+ADD CONSTRAINT [FK_UserMovie]
+    FOREIGN KEY ([User_Id])
+    REFERENCES [dbo].[UserSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserMovie'
+CREATE INDEX [IX_FK_UserMovie]
+ON [dbo].[MovieSet]
+    ([User_Id]);
+GO
+
+-- Creating foreign key on [Movie_Id] in table 'NoteSet'
+ALTER TABLE [dbo].[NoteSet]
+ADD CONSTRAINT [FK_MovieNote]
+    FOREIGN KEY ([Movie_Id])
+    REFERENCES [dbo].[MovieSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_MovieNote'
+CREATE INDEX [IX_FK_MovieNote]
+ON [dbo].[NoteSet]
+    ([Movie_Id]);
+GO
+
+-- Creating foreign key on [Movie_Id] in table 'CommentSet'
+ALTER TABLE [dbo].[CommentSet]
+ADD CONSTRAINT [FK_MovieComment]
+    FOREIGN KEY ([Movie_Id])
+    REFERENCES [dbo].[MovieSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_MovieComment'
+CREATE INDEX [IX_FK_MovieComment]
+ON [dbo].[CommentSet]
+    ([Movie_Id]);
+GO
+
+-- Creating foreign key on [Type_Id] in table 'MovieSet'
+ALTER TABLE [dbo].[MovieSet]
+ADD CONSTRAINT [FK_TypeMovie]
+    FOREIGN KEY ([Type_Id])
+    REFERENCES [dbo].[TypeSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_TypeMovie'
+CREATE INDEX [IX_FK_TypeMovie]
+ON [dbo].[MovieSet]
+    ([Type_Id]);
+GO
+
+-- Creating foreign key on [User_Id] in table 'NoteSet'
+ALTER TABLE [dbo].[NoteSet]
+ADD CONSTRAINT [FK_UserNote]
+    FOREIGN KEY ([User_Id])
+    REFERENCES [dbo].[UserSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserNote'
+CREATE INDEX [IX_FK_UserNote]
+ON [dbo].[NoteSet]
+    ([User_Id]);
+GO
+
+-- Creating foreign key on [User_Id] in table 'CommentSet'
+ALTER TABLE [dbo].[CommentSet]
+ADD CONSTRAINT [FK_UserComment]
+    FOREIGN KEY ([User_Id])
+    REFERENCES [dbo].[UserSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserComment'
+CREATE INDEX [IX_FK_UserComment]
+ON [dbo].[CommentSet]
+    ([User_Id]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
